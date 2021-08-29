f2e44c3d86c565d67ec88f84c7b7a6dd38038bbf
Jane Jeon <me@janejeon.dev>
2021-08-28 23:15:46 -0400

    the mocks weren't being loaded anyway

--- __mocks__/dns/promises.js
+++
@@ -1,6 +0,0 @@
-// get http://localhost to resolve to something for testing purposes!
-export async function lookup(hostname) {
-  return hostname === 'http://localhost'
-    ? { address: '127.0.0.1', family: 4 }
-    : { address: '1.1.1.1', family: 4 }
-}
